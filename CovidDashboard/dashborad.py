e66119c07f85a706698b23ff4790af2c07d54c80
JacasMiran <rodolforafael15@gmail.com>
2022-07-01 13:32:57 -0300

    dataframe created

--- CovidDashboard/dashborad.py
+++ CovidDashboard/dashborad.py
@@ -1,6 +1,6 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+import dash as dcc
+from dash import html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
@@ -11,6 +11,48 @@ import json
 import glob
 import os
 
-all_files = glob.glob("data/*.csv")
+### Code used to create a smaller dataset ###
+#all_files = glob.glob("data/*.csv")
+#
+#df = pd.concat((pd.read_csv(f, sep=";")) for f in all_files)
+#
+#df_states = df[(~df["estado"].isna()) & (df["codmun"].isna())]
+#df_brasil = df[df["regiao"] == "Brasil"]
+#
+#df_states.to_csv("df_states.csv")
+#df_brasil.to_csv("df_brasil.csv")
 
-df = pd.concat((pd.read_csv(f, sep=";")) for f in all_files)
+### 
+
+#df_states = pd.read_csv("df_states.csv")
+df_brasil = pd.read_csv("df_brasil.csv")
+
+df_states_ = df_states[df_states["data"]=="2020-05-13"]
+
+brazil_states =json.load(open("geojson/brazil_geo.json", "r"))
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+fig = px.choropleth_mapbox(df_states,locations="estado",
+                            center={"lat":16.95, "lon":-47.78},
+                            color="casosNovos",
+                            geojson=brazil_states,
+                            color_continuous_scale="Redor",
+                            opacity=0.4,
+                            hover_data={"casosAcumulado":True, 
+                                "casosNovos":True,
+                                "obitosNovos": True,
+                                "estado":True})
+
+#Layout
+
+app.layout = dbc.Container(
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id="choropleth-map", figure=fig)
+        ])
+    ])
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
