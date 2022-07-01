ba0edd4c6ec6d7ac9832c4a0ff7c9b0f2f4eee3e
JacasMiran <rodolforafael15@gmail.com>
2022-07-01 14:22:51 -0300

    Graph

--- CovidDashboard/dashborad.py
+++ CovidDashboard/dashborad.py
@@ -1,5 +1,5 @@
 import dash
-import dash as dcc
+import dash_core_components as dcc
 from dash import html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
@@ -28,30 +28,51 @@ df_states = pd.read_csv("df_states.csv")
 df_brasil = pd.read_csv("df_brasil.csv")
 
 df_states_ = df_states[df_states["data"]=="2020-05-13"]
-
 brazil_states =json.load(open("geojson/brazil_geo.json", "r"))
+df_data = df_states[df_states["estado"]=="RJ"]
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 fig = px.choropleth_mapbox(df_states_,locations="estado",
-                            center={"lat":16.95, "lon":-47.78},
+                            center={"lat":-11.42, "lon":-51.73},
                             color="casosNovos",
                             geojson=brazil_states,
                             color_continuous_scale="Redor",
+                            zoom=4,
                             opacity=0.4,
                             hover_data={"casosAcumulado":True, 
                                 "casosNovos":True,
                                 "obitosNovos": True,
                                 "estado":True})
 
+fig2 = go.Figure(layout={"template":"plotly_dark"})
+fig2.add_trace(go.Scatter(x=df_data["data"], y=df_data["casosAcumulado"]))
+fig2.update_layout(
+    paper_bgcolor="#242424",
+    plot_bgcolor="#242424",
+    autosize=True,
+    margin=dict(l=0, r=10,t=10, b=10)
+)
+
 #Layout
 
 app.layout = dbc.Container(
     dbc.Row([
         dbc.Col([
+            dcc.Graph(id="line-graph", figure=fig2)
+        ]),
+        dbc.Col([
             dcc.Graph(id="choropleth-map", figure=fig)
         ])
     ])
+)
+
+fig.update_layout(
+    paper_bgcolor="#242424",
+    autosize = True,
+    mapbox_style = "carto-darkmatter",
+    margin = go.Margin(l=0, r=0, t=0, b=0),
+    showlegend=False
 )
 
 if __name__ == "__main__":
